fb3c035e6557fff532e649f1d1732cd0a571634f
Markus Douglas Nascimento siqueira <makusdouglas@gmail.com>
2021-02-24 10:19:37 -0300

    initial commit

---
+++ lib/rocketpay_web/controllers/welcome_controller.ex
@@ -0,0 +1,14 @@
+defmodule RocketpayWeb.WelcomeController do
+
+
+
+
+
+
+
+
+  use RocketpayWeb, :controller
+  def index(conn, _params) do
+    text(conn, "Welcome to the RocketPay API")
+  end
+end
